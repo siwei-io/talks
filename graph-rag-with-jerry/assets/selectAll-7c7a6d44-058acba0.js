74afd593477c0b768c750d7d181895a6f6df1973
Wey Gu <weyl.gu@gmail.com>
2023-06-30 23:21:26 +0800

    fixed typo

---
+++ graph-rag-with-jerry/assets/selectAll-7c7a6d44-058acba0.js
@@ -0,0 +1,1 @@
+import{b9 as t,ba as n,bb as o}from"./index-280a3a9a.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{a as s};
--- graph-rag-with-jerry/assets/selectAll-7c7a6d44-e7975941.js
+++
@@ -1,1 +0,0 @@
-import{b9 as t,ba as n,bb as o}from"./index-b76a96d2.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{a as s};
