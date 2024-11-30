0d2ce395a7d07aa8403b699189328170276cf8c1
Wey Gu <weyl.gu@gmail.com>
2024-11-30 12:13:10 +0800

    feat: shenzhen meetup

---
+++ meetup_shenzhen_2024/assets/rag_paradiam_needle_connecting_dots-B3tnATPD.js
@@ -0,0 +1,1 @@
+const e="/talks/meetup_shenzhen_2024/assets/rag_paradiam_needle_connecting_dots-Ht4s6Q5c.png";export{e as _};
---
+++ meetup_shenzhen_2024/assets/rag_paradigm-BKriji7N.js
@@ -0,0 +1,1 @@
+const s="/talks/meetup_shenzhen_2024/assets/rag_paradigm-BX9lXqev.png";export{s as _};
