747ceb9edab702945d321f3daf12d8d404ab4ac5
Wey Gu <weyl.gu@gmail.com>
2024-12-17 16:32:13 +0800

    feat: webank meetup

---
+++ webank2024/assets/slidev/default-Camat95W.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-By-9xnor.js";import{_ as r}from"../index-CFNzWBTR.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
